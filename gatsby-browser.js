547dd3da91bb37567b12f11b67446cc175f7e933
Tyler <tyler@known.design>
2019-02-04 17:50:28 -0800

    try picturefill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+// require("picturefill/dist/plugins/mutation/pf.mutation");
+// const picturefill = require("picturefill");
 
-// You can delete this file if you're not using it
+exports.onClientEntry = async () => {
+  const picturefill = await import("picturefill");
+  await import("picturefill/dist/plugins/mutation/pf.mutation");
+  picturefill.default();
+};
+
+// exports.onRouteUpdate = () => picturefill();
